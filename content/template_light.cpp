6f72e70047cc248eada9d8e8ae674612b59f6e90
Matthew Agar <mattagar6@gmail.com>
2021-10-01 18:08:49 -0400

    Update template_light.cpp

--- content/template_light.cpp
+++ content/template_light.cpp
@@ -1,17 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define sz(x) ((int)(x.size()))
-#define all(x) x.begin(), x.end()
-#define rep(i,a,b) for (int i = a; i < int(b); i++)
 #define F first
 #define S second
-#define pb push_back
-#define eb emplace_back
 
 typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int,int> pii;
 
 
 
