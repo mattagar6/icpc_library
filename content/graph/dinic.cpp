50ce0ac221aad4b03a9346034b6b527962ec6302
Matt Agar <mattagar6@gmail.com>
2021-10-04 17:19:07 -0400

    Added common flow ideas

--- content/graph/dinic.cpp
+++ content/graph/dinic.cpp
@@ -48,3 +48,64 @@ struct Dinic {
 		return tot;
 	}
 };
+
+/*
+	Flow Tricks and Common Ideas:
+		- Max Flow = Min Cut
+		- Doing some operation on a grid -> Min Cut
+			- Example problem: https://dmoj.ca/problem/nwerc11f
+			- Submission: https://pastebin.com/ced8JmG0
+	
+	Max Flow with lower bounds on edge capacities (Flows with demands):
+		- See link: https://cp-algorithms.com/graph/flow_with_demands.html
+		- Example problem: https://dmoj.ca/problem/wac4p6
+		- Submission: https://pastebin.com/VmJ40jFc
+		
+		- Assume the flow through each edge has to be a certain amount (its demand),
+		  The flow though every edge e has to satisfy:
+			d(e) <= f(e) <= c(e), where d = demand, f = flow, c = capactiy
+		
+		- What are the MINIMUM and MAXIMUM flows from S to T that satisfy these requirements?
+		
+		- finding some ARBITRARY flow:
+			- Create a new graph with a new source (S2) and a new sink (T2).
+			- Draw an edge from S2 to all nodes in the original graph with capacity
+			  c'({S2,v}) = sum of all d({u,v}) for all edges {u,v} in original graph (sum of input demands to v)
+			- Draw an edge from each node in the original graph to T2 with capacity
+			  c'({v,T2}) = sum of all d({v,u}) for all edges {v,u} in original graph (sum of output demands from v)
+			- For each edge {u,v} in the old graph, draw edge {u,v} in the new graph with capacity
+			  c'({u,v}) = c({u,v}) - d({u,v}) (extra flow we can send between u and v)
+			- Finally, draw and edge {T,S} in the new graph with capacity INF
+			
+			- *** There exists a solution to the problem above iff the max flow from S2 -> T2 "saturates" all edges
+			  coming out of S2 in the new graph (they all end up with capacity 0 at the end) ***
+			
+			- INTUITION: 
+				- The outgoing edges from S2 "force" each node v to have the correct amount of flow coming out of them
+				  (at least the amount from the demand constraints)
+				- The incoming edges to T2 "force" each node v to have the correct amount of flow coming into them
+				  (at least the amount from the demand constraints)
+				- Imagine a path with d({u,v}) flow being pushed along it in the original graph. It's path looks like
+					
+					S -> (..P1..) -> u -> v -> (..P2..) -> T. in the new graph, its path looks like
+					S2 -> v -> (..P2..) -> T -> S -> (..P1..) -> u -> T2
+					
+					This shows why the T -> S edge is needed in the new graph (with capacity INF)
+		
+		- From now on I assume there is a solution to the problem abive.
+		
+		- finding the MAXIMUM flow:
+			- Create the Graph above. 
+			- Run max_flow(S2, T2). This will saturate all edges from S2, and all edges going to T2
+			- Run max_flow(S, T) on the residual graph from above. This flow is the answer.
+				- We have ensured that all demands are met, now we can add on as much flow as possible.
+		
+		- finding the MINIMUM flow:
+			- Create the Graph above, but DONT ADD THE EDGE T -> S!!!
+			- The answer is sum(d(e) for all e) - max_flow(S2, T2).
+				- The flow in the original graph is forced through the edge T -> S in the new graph. 
+				- Thus, the minimum possible flow in the original graph is the minimum capacity
+				  of the T -> S edge in the new graph such that there is a solution.
+					- This is exactly what the expression above gives.
+
+*/
