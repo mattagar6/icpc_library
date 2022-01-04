2ad9e1068011960a0dfbdaa1ee3665f3ccf161bd
Matt Agar <mattagar6@gmail.com>
2022-01-04 15:13:06 -0500

    update template

--- content/template.cpp
+++ content/template.cpp
@@ -10,10 +10,17 @@ const char nl = '\n';
 const int MX = 2e5 + 10;
 
 
+void solve() {
+
+}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	
-	
+	int te = 1;
+//	cin >> te;
+	while (te--) {
+		solve();
+	}
 }
