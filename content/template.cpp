b471029ef243a5c714e6f668f9bc6cb3787cc67a
Matt Agar <mattagar6@gmail.com>
2022-01-04 19:32:24 -0500

    update template

--- content/template.cpp
+++ content/template.cpp
@@ -1,26 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 #define sz(x) ((int)(x.size()))
 #define all(x) x.begin(), x.end()
- 
+
 #ifdef LOCAL
 template<typename T> void pr(T a){std::cerr<<a<<std::endl;}
 template<typename T, typename... Args> void pr(T a, Args... args){std::cerr<<a<<' ',pr(args...);}
 #else
 template<typename... Args> void pr(Args... args){}
 #endif
- 
+
 using ll = long long;
- 
+
 const char nl = '\n';
 const int MX = 2e5 + 10;
- 
- 
+
+
 void solve() {
 	
 }
- 
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
