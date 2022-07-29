4995df2ac649f2c459e5281934daff405a6551a2
Matt Agar <mattagar6@gmail.com>
2022-07-29 10:24:05 -0400

    remove debug stuff from template

--- content/templates/template.cpp
+++ content/templates/template.cpp
@@ -4,13 +4,6 @@ using namespace std;
 #define sz(x) ((int)(x.size()))
 #define all(x) x.begin(), x.end()
 
-#ifdef LOCAL
-template<typename T> void pr(T a){std::cerr<<a<<std::endl;}
-template<typename T, typename... Args> void pr(T a, Args... args){std::cerr<<a<<' ',pr(args...);}
-#else
-template<typename... Args> void pr(Args... args){}
-#endif
-
 using ll = long long;
 
 const char nl = '\n';
